f581542180ccfb0eb200a34f507706abdd89b9c3
vagrant <vagrant@vagrant.vm>
2018-11-01 17:10:00 +0000

    Corrigiendo incompatibilidades con la nueva version python 3.6.6 en el test basico

--- test_proyecto.py
+++ test_proyecto.py
@@ -9,14 +9,14 @@ class TestProyecto(unittest.TestCase):
         self.day=1
         self.month=2
         self.year=2000
-        self.cita=Cita(01,02,2000)
+        self.cita=Cita(1,2,2000)
     def testCreacionCalendario(self):
         self.assertIsInstance(self.calendario,Calendario,"Objeto calendario creado correctamente")
     def testCreacionCita(self):
         self.assertIsInstance(self.cita,Cita,"Objeto cita creado correctamente")
     def testContenidoCalendario(self):
         self.assertIsInstance(self.calendario.dOcuped,list,"Lista de dias ocupados creado correctamente")
-    
+
 
 if __name__ == "__main__":
     unittest.main()
